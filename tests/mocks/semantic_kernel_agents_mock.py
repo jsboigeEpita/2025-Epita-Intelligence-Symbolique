50854bad2d52fadd197b50cfa4256f986db87b72
jsboigeEpita <jean-sylvain.boige@epita.fr>
2025-05-27 02:35:07 +0200

    feat: Correction finale 22 tests - 84.5% atteint

---
+++ tests/mocks/semantic_kernel_agents_mock.py
@@ -0,0 +1,25 @@
+"""
+Mock pour semantic_kernel.agents
+"""
+
+class MockChatCompletionAgent:
+    def __init__(self, *args, **kwargs):
+        self.service_id = kwargs.get('service_id', 'mock_service')
+        self.kernel = kwargs.get('kernel', None)
+        self.name = kwargs.get('name', 'mock_agent')
+    
+    async def invoke(self, *args, **kwargs):
+        return "Mock response from ChatCompletionAgent"
+
+class MockAgent:
+    def __init__(self, *args, **kwargs):
+        self.name = kwargs.get('name', 'mock_agent')
+    
+    async def invoke(self, *args, **kwargs):
+        return "Mock response from Agent"
+
+# Simulation du module agents
+agents = type('agents', (), {
+    'ChatCompletionAgent': MockChatCompletionAgent,
+    'Agent': MockAgent
+})()
