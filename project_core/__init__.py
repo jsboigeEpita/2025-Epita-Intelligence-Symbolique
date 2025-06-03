4aa4ce20b71f15490f61f516f9e59af9faca2546
jsboigeEpita <jean-sylvain.boige@epita.fr>
2025-06-03 06:11:10 +0200

    Refactor: Move files from project_core to argumentation_analysis

--- project_core/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Core package for the project.
-
-This package contains the functional core of the application,
-including utilities, pipelines, and other essential modules.
-It serves as the primary entry point for accessing the project's
-core functionalities.
-"""
-# Initializer for the project_core package
