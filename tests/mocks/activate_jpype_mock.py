1940b0c9634e846e93a544457bdf612e38ec622c
jsboige <jsboige@gmail.com>
2025-06-09 17:57:45 +0200

    🔐 CLEAN FINAL: Suppression définitive de tous les mocks sur main

--- tests/mocks/activate_jpype_mock.py
+++
@@ -1,9 +0,0 @@
-"""
-Script d'activation du mock JPype.
-À importer avant toute utilisation de JPype dans les tests.
-"""
-
-# Importer le mock pour l'activer
-from tests.mocks import jpype_mock
-
-print("Mock JPype activé")
