70bbeabcf59635531ff5138eb61bb84ee4bb36a4
jsboigeEpita <jean-sylvain.boige@epita.fr>
2025-06-16 01:28:15 +0200

    feat(refactor): Fusionne stash@{0} - refactoring des stratégies d'orchestration

--- argumentation_analysis/orchestration/__init__.py
+++ argumentation_analysis/orchestration/__init__.py
@@ -6,7 +6,6 @@ Système d'orchestration multi-agents avec Oracle authentique
 from .cluedo_extended_orchestrator import CluedoExtendedOrchestrator
 from .strategies import CyclicSelectionStrategy, OracleTerminationStrategy
 
-
 __all__ = [
     "CluedoExtendedOrchestrator",
     "CyclicSelectionStrategy",
