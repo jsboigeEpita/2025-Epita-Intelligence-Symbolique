101b50a5fed454c17b2f1eef040165d83eacd930
jsboigeEpita <jean-sylvain.boige@epita.fr>
2025-06-16 03:46:18 +0200

    Refactor: Remove semantic_kernel_compatibility references

--- argumentation_analysis/scripts/simulate_balanced_participation.py
+++ argumentation_analysis/scripts/simulate_balanced_participation.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from typing import Dict, List, Tuple
 # CORRECTIF COMPATIBILITÉ: Utilisation du module de compatibilité
-from argumentation_analysis.utils.semantic_kernel_compatibility import Agent
+from semantic_kernel.agents import Agent
 from semantic_kernel.contents import ChatMessageContent, ChatRole as AuthorRole
 from unittest.mock import MagicMock
 
