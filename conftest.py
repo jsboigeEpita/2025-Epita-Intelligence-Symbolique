c952ccf564fbfe232b61d276c3da276dfd81fe23
jsboigeEpita <jean-sylvain.boige@epita.fr>
2025-06-26 15:49:28 +0200

    refactor(project): Finalize root cleanup by moving conftest and updating gitignore

--- conftest.py
+++
@@ -1,4 +0,0 @@
-# This file allows for custom pytest configuration.
-
-# A list of files or directories to be ignored during test collection.
-collect_ignore = ["tests/integration/services/test_mcp_server_integration.py"]
